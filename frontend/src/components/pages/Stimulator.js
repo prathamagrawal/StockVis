99bb4e8b14e261382b54abc7ea85fad7d0ab4195
Pratham Agrawal <prathamagrawal1205@gmail.com>
2023-08-29 01:06:54 +0530

    Cosmetic changes

--- frontend/src/components/pages/Stimulator.js
+++ frontend/src/components/pages/Stimulator.js
@@ -1,50 +1,19 @@
 import React from 'react';
-import { Flex, Button, useColorMode, Box, Image } from '@chakra-ui/react';
+import { Box, Flex, Image } from '@chakra-ui/react';
 import StockQuote from './StockQuote';
+import StockVis from '../images/Logo.svg';
 
-const Stimulator = () => {
-    const { colorMode, toggleColorMode } = useColorMode();
-    const isDarkMode = colorMode === 'dark';
-
+function Stimulator() {
     return (
-        <Flex
-            // width="100vw"
-            // height="100vh"
-            justifyContent="center"
-            alignItems="center"
-        >
-            <Flex
-                flexDirection="row"
-                justifyContent="center"
-                alignItems="center"
-                bg={isDarkMode ? 'rgba(48, 48, 48, 0.5)' : 'rgba(255, 255, 255, 0.5)'}
-                backdropFilter="blur(10px)"
-                borderRadius="md"
-                boxShadow="lg"
-                p={6}
-            >
-                {/* Left Part */}
-                <Box width="50%" pr={4}>
-                    <Button
-                        size="lg"
-                        colorScheme="blue"
-                        variant="outline"
-                        width="100%"
-                        leftIcon={<span role="img" aria-label="Toggle">{isDarkMode ? '🌙' : '☀️'}</span>}
-                        onClick={toggleColorMode}
-                    >
-                        Dark Mode
-                    </Button>
-                    <StockQuote />
-                </Box>
-
-                {/* Right Part */}
-                <Box width="50%" pl={4}>
-                    <Image src="/path-to-your-image.jpg" alt="Image" />
-                </Box>
-            </Flex>
+        <Flex minHeight="100vh" justifyContent="center" alignItems="center">
+            <Box flex="1">
+                <StockQuote />
+            </Box>
+            <Box flex="1">
+                <Image src={StockVis} alt="Your Image" w="50%" h="50%" />
+            </Box>
         </Flex>
     );
-};
+}
 
 export default Stimulator;
