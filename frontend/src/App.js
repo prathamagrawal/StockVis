52a040e4a2ff78ed47b98836a76b21dfc29f4aac
Pratham Agrawal <prathamagrawal1205@gmail.com>
2023-08-28 11:49:41 +0530

    Added stimulator UI and form

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,9 +2,9 @@ import React, { useState, useEffect } from 'react';
 import Loader from './components/Loader/Loader';
 import Home from './components/Home';
 import Stimulator from './components/pages/Stimulator';
+import StockQuote from './components/pages/StockQuote';
 import { Box } from '@chakra-ui/react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-
 
 
 function App() {
@@ -24,6 +24,7 @@ function App() {
           <Routes>
             <Route path="/" element ={<Home/>}></Route>
             <Route path="/stimulator" element ={<Stimulator/>}></Route>
+            <Route path="/StockQuote" element ={<StockQuote/>}></Route>
           </Routes>
         )}
       </Box>
--- frontend/src/components/pages/Stimulator.js
+++ frontend/src/components/pages/Stimulator.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { Flex, Button, useColorMode } from '@chakra-ui/react';
+import { Flex, Button, useColorMode, Box, Image } from '@chakra-ui/react';
+import StockQuote from './StockQuote';
 
 const Stimulator = () => {
     const { colorMode, toggleColorMode } = useColorMode();
@@ -7,26 +8,41 @@ const Stimulator = () => {
 
     return (
         <Flex
-            width="300px"
-            height="300px"
-            flexDirection="column"
+            width="100vw"
+            height="100vh"
             justifyContent="center"
             alignItems="center"
-            bg={isDarkMode ? 'rgba(48, 48, 48, 0.5)' : 'rgba(255, 255, 255, 0.5)'}
-            backdropFilter="blur(10px)"
-            borderRadius="md"
-            boxShadow="lg"
         >
-            <Button
-                size="lg"
-                colorScheme="blue"
-                variant="outline"
-                width="200px"
-                leftIcon={<span role="img" aria-label="Toggle">{isDarkMode ? '🌙' : '☀️'}</span>}
-                onClick={toggleColorMode}
+            <Flex
+                flexDirection="row"
+                justifyContent="center"
+                alignItems="center"
+                bg={isDarkMode ? 'rgba(48, 48, 48, 0.5)' : 'rgba(255, 255, 255, 0.5)'}
+                backdropFilter="blur(10px)"
+                borderRadius="md"
+                boxShadow="lg"
+                p={6}
             >
-            Dark Mode
-            </Button>
+                {/* Left Part */}
+                <Box width="50%" pr={4}>
+                    <Button
+                        size="lg"
+                        colorScheme="blue"
+                        variant="outline"
+                        width="100%"
+                        leftIcon={<span role="img" aria-label="Toggle">{isDarkMode ? '🌙' : '☀️'}</span>}
+                        onClick={toggleColorMode}
+                    >
+                        Dark Mode
+                    </Button>
+                    <StockQuote />
+                </Box>
+
+                {/* Right Part */}
+                <Box width="50%" pl={4}>
+                    <Image src="/path-to-your-image.jpg" alt="Image" />
+                </Box>
+            </Flex>
         </Flex>
     );
 };
